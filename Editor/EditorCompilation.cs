a001f79dbfca4a0d9646cffc393e062698dc2660
Sneh Raval <snehraval@mplgaming.com>
2025-09-18 14:04:44 +0530

    Editor compilation utils added use for CD CI to check whether Project have any compilation issues or not before proceeding build

---
+++ Editor/EditorCompilation.cs
@@ -0,0 +1,25 @@
+﻿using UnityEditor;
+using UnityEditor.Compilation;
+using UnityEngine;
+using System.Linq;
+
+[InitializeOnLoad]
+public class EditorCompilation
+{
+    public static bool isCompilerErr = false;
+
+    static EditorCompilation()
+    {
+        isCompilerErr = false;
+        if (Application.isBatchMode)
+            CompilationPipeline.assemblyCompilationFinished += ProcessBatchModeCompileFinish;
+    }
+
+    private static void ProcessBatchModeCompileFinish(string s, CompilerMessage[] compilerMessages)
+    {
+        if (compilerMessages.Count(m => m.type == CompilerMessageType.Error) > 0)
+        {
+            isCompilerErr = true;
+        }
+    }
+}
